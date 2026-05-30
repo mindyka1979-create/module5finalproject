f2553844254cc83979e186f903142f4037e1487e
Mindy Armour <mindyka1979@gmail.com>
2026-05-30 19:26:56 -0400

    YourMessage

---
+++ index.js
@@ -0,0 +1,8 @@
+// API 1: "https://jsonplaceholder.typicode.com/users"
+// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")
+    const usersData = awaitusers.json();
+console.log(usersData);
+}
